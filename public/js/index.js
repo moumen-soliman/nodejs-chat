51de01e6f93087dbb471ac75d7dfeedc30adb08e
moumensoliman <moumensoliman@gmail.com>
2017-10-19 13:07:45 +0200

    convert to dynamic
    add `socket.emit('createMessage', {form: 'Moumensoliman@gmail.com', text: 'hello from console side'});` in console and you will reseve new message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Ahmed',
-		text: 'Yup, that works'
-	});
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -13,15 +13,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => { //related to socket in html file
 	console.log('New user connected');
-
-	socket.emit('newMessage', {
-		from: 'Mohamed',
-		text: 'See you then',
-		createdAt: 123123
-	});
-
+	
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
