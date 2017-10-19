0ce07948c60b1c7a7eeb2bba977441f7e2915341
moumensoliman <moumensoliman@gmail.com>
2017-10-19 15:11:39 +0200

    messages from one to one

--- server/server.js
+++ server/server.js
@@ -13,14 +13,37 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => { //related to socket in html file
 	console.log('New user connected');
-	
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
+
+		//sockit.emit from admin and text
+		socket.emit('newMessage', {
+			from: 'Admin',
+			text: 'Welcome to the chat'
+			createdAt: new Date().getTime()
+		});
+		
+		//socket.broadcast.emit from admin text new user joined
+		socket.broadcast.emit('newMessage', {
+			from: 'Admin',
+			text: 'New user join now',
+			createdAt: new Date().getTime()
+		});
+
+
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+
+		//send message from one way and reseve it from one way , so open two tabbs and check
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {
