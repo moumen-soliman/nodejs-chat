830931a9e9837c0292de096bea384c8c52d087ac
moumensoliman <moumensoliman@gmail.com>
2017-10-20 22:12:55 +0200

    send location by geolocation api automatically with message

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -28,6 +28,10 @@ io.on('connection', (socket) => { //related to socket in html file
 		// 	text: message.text,
 		// 	createdAt: new Date().getTime()
 		// });
+	});
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
 	});
 
 	socket.on('disconnect', () => {
